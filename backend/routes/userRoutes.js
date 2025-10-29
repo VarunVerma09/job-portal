2a59b3fbfb9262ff0f7bd4bd4f60277297baebdf
VarunVerma09 <umangsoni409@gmail.com>
2025-10-29 20:38:04 +0530

    Routing Setup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,9 +4,9 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
-router.route("/login").post(login);
-router.route("/porfile/update").post(isAuthenticated,updateProfile);
+router.post("/register",register);
+router.post("/login",login);
+router.post("/porfile/update",isAuthenticated,updateProfile);
 
 
 export default router;
